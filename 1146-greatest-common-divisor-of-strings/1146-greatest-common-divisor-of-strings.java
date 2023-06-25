3537b83525b65bcf61538a981fe3750aa915ddbb
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-06-25 10:51:30 +0530

    Time: 40 ms (5%), Space: 44.6 MB (5.98%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String gcdOfStrings(String s1,String s2) {
+        String res="";
+        List<String>op=new ArrayList<>();
+        int i=0;
+        while(i<s2.length()){
+            res+=s2.charAt(i);
+            if(makeEqual(res,s2))  {
+                op.add(res);
+            }
+            i++;
+        }
+        System.out.println(op);
+        int maxLength=-1;
+        String ans="";
+        for(String r:op){
+            if(makeEqual(r,s1)) {
+                maxLength=Math.max(maxLength,ans.length());
+                if(maxLength==ans.length())         ans=r;
+            }
+        }             
+        return ans;
+    }
+
+    public static boolean makeEqual(String res,String s){
+        String temp="";
+        int div=s.length()/res.length();
+        for(int i=0;i<div;i++){
+            temp=temp+res;
+        }
+        if(temp.equals(s)) {
+            System.out.println(temp+" "+res);
+            return true;
+        }
+        return false;
+    }
+}
