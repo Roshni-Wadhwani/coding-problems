f5b4e54b0a0c1bd1fb8f654105752ca9f8617ecc
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-06-27 19:41:50 +0530

    Time: 0 ms (100%), Space: 41.4 MB (79.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,46 +10,71 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-        //if we are given heads of two list 
-        //we can try searching if current.next is equal to null and get the last element
-        //as soon as we get the last element we can connect that element to the first of next list
-    ListNode head3=null;
-	  ListNode temp3=null;
-		if(head1==null)			return head2;
-		else if(head2==null)		return head1;
-	  else if(head1!=null && head2!=null && head1.val<head2.val){
-	    head3=head1;
-	    temp3=head1;
-	    head1=head1.next;
-	  }
-	  else if(head1!=null && head2!=null && head1.val>=head2.val){
-	    head3=head2;
-	    temp3=head2;
-	    head2=head2.next;
-	  }
+			//Using extra space
+    // ListNode head3=null;
+	  // ListNode temp3=null;
+		// if(head1==null)			return head2;
+		// else if(head2==null)		return head1;
+	  // else if(head1!=null && head2!=null && head1.val<head2.val){
+	  //   head3=head1;
+	  //   temp3=head1;
+	  //   head1=head1.next;
+	  // }
+	  // else if(head1!=null && head2!=null && head1.val>=head2.val){
+	  //   head3=head2;
+	  //   temp3=head2;
+	  //   head2=head2.next;
+	  // }
 	   
-	  while(temp3!=null && head1!=null && head2!=null){
-	    if(head1.val<head2.val){
-	      temp3.next=head1;
-	      head1=head1.next;
-	    }
-	    else{
-	      temp3.next=head2;
-	      head2=head2.next;
-	    }
-	    temp3=temp3.next;
-	  }
+	  // while(temp3!=null && head1!=null && head2!=null){
+	  //   if(head1.val<head2.val){
+	  //     temp3.next=head1;
+	  //     head1=head1.next;
+	  //   }
+	  //   else{
+	  //     temp3.next=head2;
+	  //     head2=head2.next;
+	  //   }
+	  //   temp3=temp3.next;
+	  // }
 	  
-	  while(temp3!=null && head1!=null){
-	    temp3.next=head1;
-	    head1=head1.next;
-	    temp3=temp3.next;
-	  }
-	  while(temp3!=null && head2!=null){
-	    temp3.next=head2;
-	    head2=head2.next;
-	    temp3=temp3.next;
-	  }
-	  return head3;
+	  // while(temp3!=null && head1!=null){
+	  //   temp3.next=head1;
+	  //   head1=head1.next;
+	  //   temp3=temp3.next;
+	  // }
+	  // while(temp3!=null && head2!=null){
+	  //   temp3.next=head2;
+	  //   head2=head2.next;
+	  //   temp3=temp3.next;
+	  // }
+	  // return head3;
+
+
+
+		//without using extra space
+		if(head1==null)					return head2;
+		if(head2==null)					return head1;
+
+		if(head1.val>head2.val){
+			ListNode temp=head1;
+			head1=head2;
+			head2=temp;
+		}
+		ListNode res=head1;
+
+		while(head1!=null && head2!=null){
+			ListNode temp=null;//to keep track of last visited elem
+			while(head1!=null && head1.val<=head2.val){
+				temp=head1;
+				head1=head1.next;
+			}
+			temp.next=head2;
+			//swap so that head1->small and head2->big
+			ListNode tem=head1;
+			head1=head2;
+			head2=tem;
+		}
+		return res;
 	}
 }
