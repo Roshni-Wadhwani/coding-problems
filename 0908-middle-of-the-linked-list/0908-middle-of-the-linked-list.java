fc78924d292eca6b49753ee1a3765e63467865eb
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-06-27 13:03:00 +0530

    Time: 0 ms (100%), Space: 40 MB (83.47%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -10,18 +10,31 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int countNodes=0;
-        ListNode current=head;
-        while(current!=null){
-            countNodes++;
-            current=current.next;
+        // TC:-O(N)+O(N/2)
+        // SC:-O(1)
+        // int countNodes=0;
+        // ListNode current=head;
+        // while(current!=null){
+        //     countNodes++;
+        //     current=current.next;
+        // }
+        
+        // int mid=(countNodes)/2;
+        // mid+=1;
+        // current=head;
+        // for(int i=0;i<mid-1;i++){
+        //     current=current.next;
+        // }
+        // return current;
+
+
+        //optimised .... TORTOISE METHOD
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && slow!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        int mid=(countNodes)/2;
-        mid+=1;
-        current=head;
-        for(int i=0;i<mid-1;i++){
-            current=current.next;
-        }
-        return current;
+        return slow;
     }
 }
