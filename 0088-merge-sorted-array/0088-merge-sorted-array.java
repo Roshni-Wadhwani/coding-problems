482a6e5546450c3f169dbb5af680c8646d8ed3c4
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-06-27 19:59:12 +0530

    Time: 1 ms (34.55%), Space: 41.5 MB (56.49%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -7,14 +7,18 @@ class Solution {
         }
         int j=0;
         for(int i=0;i<m;i++){
+            //checking whether elem of nums1 is greater than nums2[0]
+            //if greater swap such that we get elements in nums1 sorted
             if(nums1[i]>nums2[j]){
                 int temp=nums1[i];
                 nums1[i]=nums2[j];
                 nums2[j]=temp;
+                //after swap, the elements are not in sorted order in nums2
                 Arrays.sort(nums2);
             }
         }
         int k=0;
+        //adding all elements of nums2 to nums1 as they are sorted
         for(int i=m;i<m+n;i++){
             nums1[i]=nums2[k];
             k++;
