2456f19e8fd12ea452ea62373a100ba9fdeb8006
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-06-28 14:43:56 +0530

    Time: 1 ms (99.98%), Space: 47.3 MB (6.89%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -11,14 +11,50 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode current1=headA; 
+        // brute force checking every node of first linked list 
+        // with every node of second to check if they are equal
+        // ListNode current1=headA; 
+        // while(current1!=null){
+        //     ListNode current2=headB;
+        //     while(current2!=null){
+        //         if(current1==current2)          return current1;
+        //         current2=current2.next;
+        //     }
+        //     current1=current1.next;
+        // }
+        // return null;
+
+
+        // OPTIMISED
+        // calculate length of first ll and second
+        // whatever is difference traverse that no of nodes
+        // both are at same index in respective ll
+        // check if they are same.. 
+        int c1=0,c2=0;
+        ListNode current1=headA;
+        ListNode current2=headB;
         while(current1!=null){
-            ListNode current2=headB;
-            while(current2!=null){
-                if(current1==current2)          return current1;
-                current2=current2.next;
-            }
+            c1++;
             current1=current1.next;
+        }
+        while(current2!=null){
+            c2++;
+            current2=current2.next;
+        }
+        current1=headA;current2=headB;
+        if(c1<c2){
+            int diff=c2-c1;
+            for(int i=0;i<diff;i++)     current2=current2.next;
+        }
+        else{
+            int diff=c1-c2;
+            for(int i=0;i<diff;i++)     current1=current1.next;
+        }
+
+        while(current1!=null||current2!=null){
+            if(current1==current2)          return current1;
+            current1=current1.next;
+            current2=current2.next;
         }
         return null;
     }
