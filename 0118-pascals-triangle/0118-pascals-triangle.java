21c248560915b38b4d991858c314f3de4a4be7cd
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-07-01 00:25:07 +0530

    Time: 26 ms (6.03%), Space: 44.1 MB (7.75%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -25,7 +25,7 @@ class Solution {
         // }
         // return ans;
 
-        // little optimised
+        // another approach
         factorial.clear();
         ans.clear();
         calculateFactorialUptoN(numRows);
