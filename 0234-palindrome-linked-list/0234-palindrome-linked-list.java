5e9c59799fa61f7eb9d83d78795dfb3ec6d106f8
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-06-30 20:46:53 +0530

    Time: 20 ms (18.27%), Space: 75.2 MB (5.6%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,29 +9,45 @@
  * }
  */
 class Solution {
+    // for 2nd approach
+    public static ListNode conjugate=null;
+
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        int i=0;
-        while(fast!=null && fast.next!=null && fast.next.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-        }
-        ListNode prev=null;
-        ListNode current=slow.next;
-        while(current!=null){
-            ListNode newNode=current.next;
-            current.next=prev;
-            prev=current;
-            current=newNode;
-        }
-        ListNode tempHead=prev;
-        ListNode initHead=head;
-        while(tempHead!=null){
-            if(tempHead.val!=initHead.val)          return false;
-            tempHead=tempHead.next;
-            initHead=initHead.next;
-        }
+        // // find middle of linkedlink and reverse right half and go 
+        // // on checking
+        // ListNode slow=head;
+        // ListNode fast=head;
+        // int i=0;
+        // while(fast!=null && fast.next!=null && fast.next.next!=null){
+        //     fast=fast.next.next;
+        //     slow=slow.next;
+        // }
+        // ListNode prev=null;
+        // ListNode current=slow.next;
+        // while(current!=null){
+        //     ListNode newNode=current.next;
+        //     current.next=prev;
+        //     prev=current;
+        //     current=newNode;
+        // }
+        // ListNode tempHead=prev;
+        // ListNode initHead=head;
+        // while(tempHead!=null){
+        //     if(tempHead.val!=initHead.val)          return false;
+        //     tempHead=tempHead.next;
+        //     initHead=initHead.next;
+        // }
+        // return true;
+
+        // another approach maintain 2 pointers current and conjugate
+        conjugate=head;
+        return goAhead(head);
+    }
+    public static boolean goAhead(ListNode node){
+        if(node==null)          return true;
+        if(!goAhead(node.next))     return false;
+        if(conjugate.val!=node.val)             return false;
+        conjugate=conjugate.next;
         return true;
     }
 }
