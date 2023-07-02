ecefc3954ad04b371f6031760ddfd76dbf44be45
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-07-02 14:57:20 +0530

    Time: 0 ms (100%), Space: 41.5 MB (39.29%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,10 +1,24 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         // brute force
+        // for(int i=0;i<matrix.length;i++){
+        //     int temp[]=matrix[i];
+        //     for(int j=0;j<temp.length;j++){
+        //         if(matrix[i][j]==target)              return true;
+        //     }
+        // }
+        // return false;
+
+
+        // little efficient
         for(int i=0;i<matrix.length;i++){
             int temp[]=matrix[i];
-            for(int j=0;j<temp.length;j++){
-                if(matrix[i][j]==target)              return true;
+            int firstEle=temp[0];
+            int lastEle=temp[temp.length-1];
+            if(target>=firstEle && target<=lastEle){
+                for(int j=0;j<temp.length;j++){
+                    if(matrix[i][j]==target)            return true;
+                }
             }
         }
         return false;
