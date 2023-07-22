eb053695ad3b2e6523eee4712adafc6d8cfb6afd
Roshni-Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-07-22 17:45:17 +0530

    Time: 2 ms (81.07%), Space: 44.4 MB (8.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,16 +2,32 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         
 //         Brute force
-        int[] ans=new int[2];
+        // int[] ans=new int[2];
+        // for(int i=0;i<nums.length;i++){
+        //     int num1=nums[i];
+        //     for(int j=i+1;j<nums.length;j++){
+        //         int num2=nums[j];
+        //         if(num1+num2==target){
+        //             ans[0]=i;
+        //             ans[1]=j;
+        //             return ans;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+//         optimal
+        HashMap<Integer,Integer>index=new HashMap<>();
+        int ans[]=new int[2];
         for(int i=0;i<nums.length;i++){
-            int num1=nums[i];
-            for(int j=i+1;j<nums.length;j++){
-                int num2=nums[j];
-                if(num1+num2==target){
-                    ans[0]=i;
-                    ans[1]=j;
-                    return ans;
-                }
+            int diff=target-nums[i];
+            if(index.containsKey(diff)){
+                ans[0]=index.get(diff);ans[1]=i;return ans;
+            }
+            else{
+                index.put(nums[i],i);
             }
         }
         return ans;
