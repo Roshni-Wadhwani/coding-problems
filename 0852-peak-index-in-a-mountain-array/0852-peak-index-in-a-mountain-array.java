9b085fee56c919c6cbcaf4497c90b7976e575973
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-07-25 11:03:21 +0530

    Time: 0 ms (100.00%), Space: 55.3 MB (55.22%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,10 +1,23 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int i=0;
-        while(arr[i]<arr[i+1]) {
-            i++;
+        
+//         linear search
+        // int i=0;
+        // while(arr[i]<arr[i+1]) {
+        //     i++;
+        // }
+        // return i;
+        
+//         binary search
+        int left=0;
+        int right=arr.length-1;
+        
+        while(left<right){
+            int mid=(left+right)/2;
+            if(arr[mid]>arr[mid+1])     right=mid;
+            else                        left=mid+1;
         }
-        return i;
+        return left;
     }
 }
 
