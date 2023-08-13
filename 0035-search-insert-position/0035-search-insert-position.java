d9205b938200c4fac7c7da6b138d04f019ecb724
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2023-08-13 18:32:51 +0530

    Time: 0 ms (100%), Space: 42.7 MB (99.05%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,13 +2,23 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
 
         // linear search
-       int i=0;
-       int n=nums.length;
-       while(i<n && nums[i]<target)        i++;
-       return i;
+    //    int i=0;
+    //    int n=nums.length;
+    //    while(i<n && nums[i]<target)        i++;
+    //    return i;
 
 
-    // binary search
+        // binary search
+        return binarySearch(0,nums.length-1,nums,target);
+    }
 
+    public static int binarySearch(int start,int end,int a[],int k){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(a[mid]==k)                   return mid;
+            if(a[mid]<k)                    start=mid+1;
+            else                            end=mid-1;
+        }
+        return start;
     }
 }
