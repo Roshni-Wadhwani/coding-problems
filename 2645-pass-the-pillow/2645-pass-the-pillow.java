e091750ddd4a720ba0e1f00712680c4b1aff98bc
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-07-07 01:45:50 +0530

    Time: 0 ms (100%), Space: 40.1 MB (73.22%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.java
+++ 2645-pass-the-pillow/2645-pass-the-pillow.java
@@ -1,10 +1,6 @@
 class Solution {
     public int passThePillow(int n, int time) {
 
-        List<Integer>arr=new ArrayList<>();
-
-        for(int i=1;i<=n;i++)       arr.add(i);
-
         int ptr=0;
         int cntr=0;
         int flag=0;
@@ -28,19 +24,5 @@ class Solution {
             cntr++;
         }
         return ptr;
-        // n=4 time=5
-        // ptr=0
-        // cntr=0
-
-        // 0<=5            true
-        // flag=0  true    
-        // 0<4         ptr=1       cntr=1
-
-        // 4<=5        true
-        // flag=0 true
-        // 4<4         
-        // 4==4        flag=1      ptr=3       cntr=5
-
-        // 5<=5 true flag=1    ptr=2       cntr=6
     }
 }
