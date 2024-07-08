6b45ee27cced13ec97a981022767fe185f0011ba
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-07-08 16:27:08 +0530

    Time: 22 ms (30.26%), Space: 40.5 MB (58.65%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.java
@@ -5,13 +5,12 @@ class Solution {
         // 0 0 0 0 0 0 0
 
         int temp=n;// to ensure n-1 times loop chalna chahiye
-        int i=1;//4
-        int cZeros=n+1;
+        int i=1;//to keep record of starting point
 
         while(temp>1){
 
-            int cntIndex=0;//ek bi index nhi mili hai 
-            int index=1;
+            int cntIndex=0;//to take into account how many elements covered so far 
+            int index=1;//to keep record of index which should be removed
 
             while(cntIndex<k ){
                 if(flag[i]==0){
@@ -19,14 +18,14 @@ class Solution {
                     index=i;//3
                 }
                 i++;
-                if(i>n)         i=i%n;
+                if(i>n)         i=i%n;//for clockwise rotation
             }
             flag[index]=-1;
             temp--;
-            // cZeros--;
         }
 
         int ans=0;
+        //to find out one index where flag=0
         for(int j=1;j<flag.length;j++){
             if(flag[j]==0)      {
                 ans=j;
