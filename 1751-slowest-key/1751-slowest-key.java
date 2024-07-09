8bf5a4827914625eec33fb9739e97c08240187d8
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-07-09 15:02:22 +0530

    Time: 47 ms (6.27%), Space: 44.8 MB (9.4%) - LeetHub

---
+++ 1751-slowest-key/1751-slowest-key.java
@@ -0,0 +1,29 @@
+class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        int diff[]=new int[releaseTimes.length];
+        diff[0]=releaseTimes[0];
+        int maxDiff=diff[0];
+
+        for(int i=1;i<releaseTimes.length;i++){
+            diff[i]=releaseTimes[i]-releaseTimes[i-1];
+            maxDiff=Math.max(maxDiff,diff[i]);
+        }
+
+        for(int i=0;i<diff.length;i++)      System.out.print(diff[i]+" ");
+
+        System.out.println("maxDiff: "+maxDiff);
+
+        char ans='$';
+        for(int i=0;i<diff.length;i++){
+            if(diff[i]==maxDiff)        {
+                if(ans=='$')        ans=keysPressed.charAt(i);
+                else                {
+                    if(ans<keysPressed.charAt(i))           ans=keysPressed.charAt(i);
+                }
+            }
+        }
+        return ans;
+        
+
+    }
+}
