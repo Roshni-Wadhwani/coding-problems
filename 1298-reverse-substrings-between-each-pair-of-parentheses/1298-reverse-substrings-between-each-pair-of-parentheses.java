744a1215b752b11fc19b3eab0aea21f1a88fe4ae
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-07-11 23:34:39 +0530

    Time: 1 ms (99.49%), Space: 41.5 MB (69.01%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
@@ -13,20 +13,8 @@ class Solution {
         }
     }
     public String reverseParentheses(String s) {
-        // (ed(et(oc))el)
-        // strt=[0,2,4]
-        // end=[5,5,7]
-
-        // string="edetocel"
-        // string.split=[e,d,e,t,o,c,e,l]
-
-
-        // 4 to 5 reverse=>[e,d,e,t,c,o,e,l]
-        // 2 to 5 reverse=>[e,d,o,c,t,e,e,l]
-        // 0 to 7 reverse=>[]
-
-        Deque<Integer> stack = new ArrayDeque<>();
-        List<Integer> pairs = new ArrayList<>(Collections.nCopies(s.length(), 0));
+Deque<Integer> stack = new ArrayDeque<>();
+        int[] pairs = new int[s.length()];
 
         // Track the matching parentheses indices
         for (int i = 0; i < s.length(); i++) {
@@ -34,22 +22,25 @@ class Solution {
                 stack.push(i);
             } else if (s.charAt(i) == ')') {
                 int start = stack.pop();
-                pairs.set(start, i);
-                pairs.set(i, start);
+                pairs[start] = i;
+                pairs[i] = start;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        int direction = 1; // 1 for forward, -1 for backward
-        for (int i = 0; i < s.length(); i += direction) {
+        int i = 0;
+        int direction = 1;
+
+        while (i < s.length()) {
             if (s.charAt(i) == '(' || s.charAt(i) == ')') {
-                i = pairs.get(i); // jump to the matching parenthesis
-                direction = -direction; // reverse the direction
+                i = pairs[i];
+                direction = -direction;
             } else {
                 sb.append(s.charAt(i));
             }
+            i += direction;
         }
 
         return sb.toString();
-    }    
+    }
 }
