aa4b390f05c96f3dbf97d0842fc8e37861bfbf33
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-07-12 14:41:37 +0530

    Time: 1992 ms (5.05%), Space: 53.6 MB (8.08%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -1,72 +1,45 @@
 class Solution {
+    public int giveAns(String s,int first,int second,Character ch1,Character ch2){
+        int ans=0;
+        String newString="";
+        Deque<Character>stack=new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            if(stack.isEmpty())         stack.push(s.charAt(i));
+            else{
+                Character ch=stack.peek();//a
+                Character toBePushed=s.charAt(i);//b
+
+                if(toBePushed==ch1 && ch==ch2){
+                    stack.pop();
+                    ans+=first;//10
+                }
+                else                stack.push(toBePushed);
+            }
+        }
+        while(!stack.isEmpty())         newString=stack.pop()+newString;
+
+        for(int i=0;i<newString.length();i++){
+            if(stack.isEmpty())         stack.push(newString.charAt(i));
+            else{
+                Character ch=stack.peek();//a
+                Character toBePushed=newString.charAt(i);//b
+
+                if(toBePushed==ch2 && ch==ch1){
+                    stack.pop();
+                    ans+=second;//10
+                }
+                else                stack.push(toBePushed);
+            }
+        }
+        return ans;
+    }
     public int maximumGain(String s, int x, int y) {
         int ans=0;
 
-        Deque<Character>stack=new ArrayDeque<>();
-        if(x>y){
-            String newString="";
-            for(int i=0;i<s.length();i++){
-                if(stack.isEmpty())         stack.push(s.charAt(i));
-                else{
-                    Character ch=stack.peek();//a
-                    Character toBePushed=s.charAt(i);//b
-
-                    if(toBePushed=='b' && ch=='a'){
-                        stack.pop();
-                        ans+=x;//10
-                    }
-                    else                stack.push(toBePushed);
-                }
-            }
-            while(!stack.isEmpty())         newString=stack.pop()+newString;
-            // System.out.println(newString);
-
-            for(int i=0;i<newString.length();i++){
-                if(stack.isEmpty())         stack.push(newString.charAt(i));
-                else{
-                    Character ch=stack.peek();//a
-                    Character toBePushed=newString.charAt(i);//b
-
-                    if(toBePushed=='a' && ch=='b'){
-                        stack.pop();
-                        ans+=y;//10
-                    }
-                    else                stack.push(toBePushed);
-                }
-            }
-        }
-        else{
-            String newString="";
-            for(int i=0;i<s.length();i++){
-                if(stack.isEmpty())         stack.push(s.charAt(i));
-                else{
-                    Character ch=stack.peek();//a
-                    Character toBePushed=s.charAt(i);//b
-
-                    if(toBePushed=='a' && ch=='b'){
-                        stack.pop();
-                        ans+=y;//10
-                    }
-                    else                stack.push(toBePushed);
-                }
-            }
-            while(!stack.isEmpty())         newString=stack.pop()+newString;
-            // System.out.println(newString);
-
-            for(int i=0;i<newString.length();i++){
-                if(stack.isEmpty())         stack.push(newString.charAt(i));
-                else{
-                    Character ch=stack.peek();//a
-                    Character toBePushed=newString.charAt(i);//b
-
-                    if(toBePushed=='b' && ch=='a'){
-                        stack.pop();
-                        ans+=x;//10
-                    }
-                    else                stack.push(toBePushed);
-                }
-            }
-        }
+       
+        if(x>y)         ans=giveAns(s,x,y,'b','a');
+        else            ans=giveAns(s,y,x,'a','b');
+        
         
         return ans;  
     }
