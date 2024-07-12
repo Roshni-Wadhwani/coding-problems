3e76258259031f8ef666a70a1557114db9b444db
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-07-12 14:29:11 +0530

    Time: 2059 ms (5.05%), Space: 58.1 MB (6.06%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -0,0 +1,77 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        //aabbaaxybbaabb
+        // a
+
+        String str[]=s.split("");
+        int ans=0;
+
+        Deque<Character>stack=new ArrayDeque<>();
+        if(x>y){
+            String newString="";
+            for(int i=0;i<s.length();i++){
+                if(stack.isEmpty())         stack.push(s.charAt(i));
+                else{
+                    Character ch=stack.peek();//a
+                    Character toBePushed=s.charAt(i);//b
+
+                    if(toBePushed=='b' && ch=='a'){
+                        stack.pop();
+                        ans+=x;//10
+                    }
+                    else                stack.push(toBePushed);
+                }
+            }
+            while(!stack.isEmpty())         newString=stack.pop()+newString;
+            // System.out.println(newString);
+
+            for(int i=0;i<newString.length();i++){
+                if(stack.isEmpty())         stack.push(newString.charAt(i));
+                else{
+                    Character ch=stack.peek();//a
+                    Character toBePushed=newString.charAt(i);//b
+
+                    if(toBePushed=='a' && ch=='b'){
+                        stack.pop();
+                        ans+=y;//10
+                    }
+                    else                stack.push(toBePushed);
+                }
+            }
+        }
+        else{
+            String newString="";
+            for(int i=0;i<s.length();i++){
+                if(stack.isEmpty())         stack.push(s.charAt(i));
+                else{
+                    Character ch=stack.peek();//a
+                    Character toBePushed=s.charAt(i);//b
+
+                    if(toBePushed=='a' && ch=='b'){
+                        stack.pop();
+                        ans+=y;//10
+                    }
+                    else                stack.push(toBePushed);
+                }
+            }
+            while(!stack.isEmpty())         newString=stack.pop()+newString;
+            // System.out.println(newString);
+
+            for(int i=0;i<newString.length();i++){
+                if(stack.isEmpty())         stack.push(newString.charAt(i));
+                else{
+                    Character ch=stack.peek();//a
+                    Character toBePushed=newString.charAt(i);//b
+
+                    if(toBePushed=='b' && ch=='a'){
+                        stack.pop();
+                        ans+=x;//10
+                    }
+                    else                stack.push(toBePushed);
+                }
+            }
+        }
+        
+        return ans;  
+    }
+}
