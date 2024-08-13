3303e501bdd57137de8e095df5b508c5f096b4de
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-08-13 13:51:59 +0530

    Time: 0 ms (100%), Space: 40 MB (76.34%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int fib(int n) {
+        //recursion
         // if(n<=1)            return n;
         // else                return fib(n-1)+fib(n-2);
+
+        //dp
 
         int dp[]=new int[n+1];
 
