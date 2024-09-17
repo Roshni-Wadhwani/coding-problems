600c161a3836551804f024079f33d7df2ac6ed98
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-09-17 13:29:51 +0530

    Time: 1 ms (99.11%), Space: 59 MB (86.31%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+
+        // brute force
+        // int diff    =   k;
+        // int stud    =   0;
+
+        // while(diff>0){
+        //     if(stud>=chalk.length)      stud=stud%chalk.length;
+        //     diff-=chalk[stud];
+        //     stud++;
+        // }
+
+        // if(diff<0)                      {
+        //     stud--;
+        // }
+
+        // return stud%chalk.length;
+
+
+        //
+        long sumArray=0;
+        for(int i=0;i<chalk.length;i++)             sumArray+=chalk[i];
+
+        long mod=k%sumArray;
+        long div=k/sumArray;
+        long nearestMultiple=sumArray*div;
+        k-=nearestMultiple;
+
+        int stud=0;
+        while(k>0){
+            if(stud>=chalk.length)      stud=stud%chalk.length;
+            k-=chalk[stud];
+            if(k<0)                     return stud;
+            stud++;
+        }
+
+        return stud%chalk.length;
+
+    }
+}
