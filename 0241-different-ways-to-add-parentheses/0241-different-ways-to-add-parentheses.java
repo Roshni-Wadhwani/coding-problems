c90121947692718715ac92999111697e6e2b60cf
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2024-09-19 13:32:52 +0530

    Time: 2 ms (68.85%), Space: 42.6 MB (52.36%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,23 +1,31 @@
 class Solution {
-    public List<Integer> diffWaysToCompute(String expression) {
+    public List<Integer>diffWaysToCompute(String expression){
+        List<Integer>[][] arr=new ArrayList[expression.length()][expression.length()];
+
+        return compute(expression,arr,0,expression.length()-1);
+    }
+    public List<Integer> compute(String expression,List<Integer>[][] arr,int start,int end) {
         List<Integer>ans=new ArrayList<>();
 
-        if(expression.length()==0)          return ans;
-        if(expression.length()==1)      {
-            ans.add(Integer.parseInt(expression));
+        if(arr[start][end]!=null)              return arr[start][end];
+
+        if(start==end)      {
+            ans.add(expression.charAt(start)-'0');
             return ans;
         }
-        if(expression.length()==2 && Character.isDigit(expression.charAt(0))){
-            ans.add(Integer.parseInt(expression));
+        if(end-start==1 && Character.isDigit(expression.charAt(start))){
+            int st=expression.charAt(start)-'0';
+            int en=expression.charAt(end)-'0';
+            ans.add(10*st + en);
             return ans;
         }
 
-        for(int i=0;i<expression.length();i++){
+        for(int i=start;i<=end;i++){
             char currentChar=expression.charAt(i);
 
             if(Character.isDigit(currentChar))      continue;
-            List<Integer>leftResults=diffWaysToCompute(expression.substring(0,i));
-            List<Integer>rightResults=diffWaysToCompute(expression.substring(i+1));
+            List<Integer>leftResults=compute(expression,arr,start,i-1);
+            List<Integer>rightResults=compute(expression,arr,i+1,end);
 
             for(int leftVal:leftResults){
                 for(int rightVal:rightResults){
