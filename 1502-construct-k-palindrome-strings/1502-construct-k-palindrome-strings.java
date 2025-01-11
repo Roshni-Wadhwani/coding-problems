7248754ac4576b93b08575e638bbeb4f55a3c2da
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2025-01-11 13:35:13 +0530

    Time: 18 ms (34.17%), Space: 45.7 MB (19.42%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        int freq[]  =   new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch =   s.charAt(i);
+            int asciiMod    =   (int)ch-97;
+            freq[asciiMod]  +=  1;
+        }
+
+        for(int i=0;i<freq.length;i++)      System.out.print(freq[i]+" ");
+        int oddC=0, evenC=0;
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]!=0)  {
+                if(freq[i]%2==0)            evenC++;
+                else                        oddC++;
+            }
+        }
+
+        if(s.length()<k)            return false;
+        if(oddC<=k)                 return true;
+        else{
+            if(s.length()%k==0){
+                for(int i=0;i<freq.length;i++)  {
+                    if(freq[i]!=k)              return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
