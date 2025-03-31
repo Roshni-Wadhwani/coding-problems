bca170e423a7449eb9ceaa41632b53774342e751
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2025-03-31 12:49:16 +0530

    Time: 39 ms (3.8%), Space: 45.2 MB (5.05%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int searchZeroIndex(int nums[],int s,int e){
+        for(int i=s;i<e;i++){
+            if(nums[i]==0)              return i;
+        }
+        return -1;
+    }
+
+    public int searchNonZeroIndex(int n[],int s,int e){
+        for(int i=s;i<e;i++){
+            if(n[i]>0)                  return i;
+        }
+        return -1;
+    }
+    public int[] applyOperations(int[] nums) {
+        int i=0;
+        int j=1;
+        int n=nums.length;
+
+        while(i<n && j<n){
+            if(nums[i]==nums[j]){
+                nums[i]*=2;
+                nums[j]=0;
+            }
+            i++;
+            j++;
+        }
+
+        for(int r=0;r<nums.length;r++)      System.out.print(nums[r]+"  ");
+
+        for(int k=0;k<nums.length;k++){
+            int zeroIndex=searchZeroIndex(nums,0,n);//2
+            int nonZeroIndex=searchNonZeroIndex(nums,k,n);//3
+
+            if(nonZeroIndex<zeroIndex) {}// do nothing
+            else{
+                if(nonZeroIndex>=0 && zeroIndex>=0){
+                    int temp=nums[nonZeroIndex];
+                    nums[nonZeroIndex]=nums[zeroIndex];
+                    nums[zeroIndex]=temp;
+                }
+                else                    return nums;
+            }
+        }
+
+        return nums;
+    }
+}
+// [1,4,0,2,0,0]
+// 2,4,5
+// 0,1,3
