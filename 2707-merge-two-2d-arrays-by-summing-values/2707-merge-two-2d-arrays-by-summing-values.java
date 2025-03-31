e83b5ee83077378c0e428fe509f092af2a6c97d5
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2025-03-31 16:17:34 +0530

    Time: 2 ms (43.25%), Space: 45.5 MB (30.67%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,32 +1,63 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        HashMap<Integer,Integer>map=new HashMap<>();
+        // HashMap<Integer,Integer>map=new HashMap<>();
+
+        // for(int i=0;i<nums1.length;i++){
+        //     map.put(nums1[i][0],nums1[i][1]);
+        // }
+
+        // for(int i=0;i<nums2.length;i++){
+        //     int k=nums2[i][0];
+        //     if(map.containsKey(k)){
+        //         int v=map.get(k);
+        //         map.put(k,v+nums2[i][1]);
+        //     }
+        //     else                            map.put(nums2[i][0],nums2[i][1]);
+        // }
+
+        // System.out.println(map);
+
+        // int[][] ans=new int[map.size()][2];
+        // int k=0;
+
+        // for(Map.Entry<Integer,Integer>m:map.entrySet()){
+        //     ans[k][0]=m.getKey();
+        //     ans[k][1]=m.getValue();
+        //     k++;
+        // }
+
+        // Arrays.sort(ans, (a,b)->Integer.compare(a[0],b[0]));
+        // return ans;
+
+
+        int[] freq = new int[2001];
+        int c=0;
 
         for(int i=0;i<nums1.length;i++){
-            map.put(nums1[i][0],nums1[i][1]);
+            int num=nums1[i][0];
+            freq[num]+=nums1[i][1];
         }
 
         for(int i=0;i<nums2.length;i++){
-            int k=nums2[i][0];
-            if(map.containsKey(k)){
-                int v=map.get(k);
-                map.put(k,v+nums2[i][1]);
+            int num=nums2[i][0];
+            freq[num]+=nums2[i][1];
+        }
+
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>0)           c++;
+        }
+
+        int ans[][]=new int[c][2];
+        int ind=0;
+
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>0)       {
+                ans[ind][0]=i;
+                ans[ind][1]=freq[i];
+                ind++;
             }
-            else                            map.put(nums2[i][0],nums2[i][1]);
         }
 
-        System.out.println(map);
-
-        int[][] ans=new int[map.size()][2];
-        int k=0;
-
-        for(Map.Entry<Integer,Integer>m:map.entrySet()){
-            ans[k][0]=m.getKey();
-            ans[k][1]=m.getValue();
-            k++;
-        }
-
-        Arrays.sort(ans, (a,b)->Integer.compare(a[0],b[0]));
         return ans;
     }
 }
