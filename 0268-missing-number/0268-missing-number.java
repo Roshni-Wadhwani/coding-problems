6d5205fff063edd7c8fed216d463c50fe6e80d7b
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2025-06-29 20:00:49 +0530

    Time: 0 ms (100%), Space: 45.7 MB (21.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -19,12 +19,22 @@ class Solution {
 
         // return checkNum;
 
-        int n = nums.length;
-        long sum = ((long)n*(n+1))/2;
-        long tempSum=0;
+        // int n = nums.length;
+        // long sum = ((long)n*(n+1))/2;
+        // long tempSum=0;
 
-        for(int i=0;i<n;i++)        tempSum+=nums[i];
+        // for(int i=0;i<n;i++)        tempSum+=nums[i];
 
-        return (int)((int)sum)-(int)tempSum;
+        // return (int)((int)sum)-(int)tempSum;
+
+
+        int xor1 = 0; // all xor from 0 to n
+        int xor2 = 0;// all xor -> all elements 
+
+        for(int i=0;i<nums.length;i++){
+            xor1=xor1^(i+1);
+            xor2=xor2^nums[i];
+        }
+        return xor1^xor2;
     }
 }
