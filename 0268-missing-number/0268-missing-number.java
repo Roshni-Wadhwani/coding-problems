c0e03b04a0802fcba239b706cfe097b60514f881
Roshni Wadhwani <102158847+Roshni-Wadhwani@users.noreply.github.com>
2025-06-29 19:52:56 +0530

    Time: 0 ms (100%), Space: 45.3 MB (71.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -9,14 +9,22 @@ class Solution {
         // return nums.length;
 
 
-        Set<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++)      set.add(nums[i]);
+        // Set<Integer> set = new HashSet<>();
+        // for(int i=0;i<nums.length;i++)      set.add(nums[i]);
 
-        int checkNum =0;
-        while(set.contains(checkNum)){
-            checkNum++;
-        }
+        // int checkNum =0;
+        // while(set.contains(checkNum)){
+        //     checkNum++;
+        // }
 
-        return checkNum;
+        // return checkNum;
+
+        int n = nums.length;
+        long sum = ((long)n*(n+1))/2;
+        long tempSum=0;
+
+        for(int i=0;i<n;i++)        tempSum+=nums[i];
+
+        return (int)((int)sum)-(int)tempSum;
     }
 }
